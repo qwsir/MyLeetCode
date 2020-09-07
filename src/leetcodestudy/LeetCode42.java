6554b147df3fc73318b17bde8d01e76ed1ff429a
qwxia <qwxiansheng@outlook.com>
2020-09-07 20:45:55 +0800

    LeetCode

--- src/leetcodestudy/LeetCode42.java
+++ src/leetcodestudy/LeetCode42.java
@@ -31,4 +31,25 @@ public class LeetCode42 {
         }
         return sum;
     }
+    //下面的方法是预先已经计算了第i列左右两边的最大值(不包括当前的第i列)
+//    public int trap(int[] height) {
+//        int sum = 0;
+//        int[] max_left = new int[height.length];
+//        int[] max_right = new int[height.length];
+//
+//        for (int i = 1; i < height.length - 1; i++) {
+//            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+//        }
+//        for (int i = height.length - 2; i >= 0; i--) {
+//            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+//        }
+//        for (int i = 1; i < height.length - 1; i++) {
+//            int min = Math.min(max_left[i], max_right[i]);
+//            if (min > height[i]) {
+//                sum = sum + (min - height[i]);
+//            }
+//        }
+//        return sum;
+//    }
+
 }
